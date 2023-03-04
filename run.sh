1b5717b355c9b3361200e0ccf3867ab3aa585fa7
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 00:32:20 +0800

    support resnet50

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 /root/miniconda3/envs/torch/bin/python main.py --task_num 5
 /root/miniconda3/envs/torch/bin/python main.py --task_num 10
+/root/miniconda3/envs/torch/bin/python main_pretrain.py --task_num 5
+/root/miniconda3/envs/torch/bin/python main_pretrain.py --task_num 10
