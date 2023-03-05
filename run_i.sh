be6181ece8be5f6d61460e48337d3fb2c658671f
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-06 00:18:11 +0800

    support resnet50

---
+++ run_i.sh
@@ -0,0 +1,5 @@
+cd Imagenet100
+/root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --task_num 5
+#/root/miniconda3/envs/torch/bin/python main_PASS_imagenet --task_num 10
+#/root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --task_num 5
+#/root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --task_num 10
