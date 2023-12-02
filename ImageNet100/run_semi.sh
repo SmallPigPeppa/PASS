efee935a82fc4799f8db849ed4b9ba11ee574e49
lwz <905328134@qq.com>
2023-12-02 20:32:03 +0800

    support resnet50

--- ImageNet100/run_semi.sh
+++ ImageNet100/run_semi.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=4 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_debug.py --fg_nc 20 --task_num 4 &
-#CUDA_VISIBLE_DEVICES=5 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --fg_nc 10 --task_num 9 &
-#CUDA_VISIBLE_DEVICES=6 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 20 --task_num 4 &
-#CUDA_VISIBLE_DEVICES=7 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 10 --task_num 9
+#CUDA_VISIBLE_DEVICES=4 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_debug.py --fg_nc 20 --task_num 4 &
+##CUDA_VISIBLE_DEVICES=5 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --fg_nc 10 --task_num 9 &
+##CUDA_VISIBLE_DEVICES=6 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 20 --task_num 4 &
+##CUDA_VISIBLE_DEVICES=7 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 10 --task_num 9
 
 
 
@@ -9,3 +9,9 @@ CUDA_VISIBLE_DEVICES=4 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet
 #CUDA_VISIBLE_DEVICES=5 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --fg_nc 10 --task_num 9 &
 #CUDA_VISIBLE_DEVICES=6 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 20 --task_num 4 &
 #CUDA_VISIBLE_DEVICES=7 /root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 10 --task_num 9
+
+
+/root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --fg_nc 20 --task_num 4
+/root/miniconda3/envs/torch/bin/python main_PASS_imagenet.py --fg_nc 10 --task_num 9
+/root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 20 --task_num 4
+/root/miniconda3/envs/torch/bin/python main_PASS_imagenet_pretrain.py --fg_nc 10 --task_num 9
